1c207f396e7ee07fa4b64b689247b7d7ce53b30d
cclauss <cclauss@bluewin.ch>
2016-03-17 20:46:49 +0100

    enumerate()

--- analysis.py
+++ analysis.py
@@ -371,17 +371,11 @@ def evaluate_pred(preds, train_true):
     N = train_true.shape[0];
     X = np.zeros((N*2,2));
     y = np.zeros(N*2);
-    i = 0;
-    for idx,row in train_true.iterrows():
+    for i,idx,row in enumerate(train_true.iterrows()):
         y[i*2] = row['Systole'];
         y[i*2+1] = row['Diastole'];
         res = preds.get(row['Id']);
-        if res is None:
-            X[i*2] = [np.nan,np.nan];
-            X[i*2+1] = [np.nan,np.nan];
-        else:
-            X[i*2] = res[0:2];
-            X[i*2+1] = res[2:4];
-        i += 1;
+        X[i*2] = res[0:2] if res else [np.nan,np.nan];
+        X[i*2+1] = res[2:4] if res else [np.nan,np.nan];
     score = crps_score(X,y);
     print("score is {}".format(score));
